65ff3c60e5bb9b1a0157519b6b11ff6c56384270
dinghenc <308024784@qq.com>
2022-05-17 22:39:54 +0800

    feat: test leetcode: 验证外星语词典

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	fmt.Println("Wiki search engine.")
+	words := []string{"hello", "leetcode"}
+	order := "hlabcdefgijkmnopqrstuvwxyz"
+	sorted := isAlienSorted(words, order)
+	fmt.Println(sorted)
+}
+
+func isAlienSorted(words []string, order string) bool {
+	alphaOrderMap := make(map[byte]int)
+	for i, c := range order {
+		alphaOrderMap[byte(c)] = i
+	}
+
+	return sort.SliceIsSorted(words, func(i, j int) bool {
+		for index := 0; index < len(words[i]) && index < len(words[j]); index++ {
+			if words[i][index] == words[j][index] {
+				continue
+			}
+			return alphaOrderMap[words[i][index]] < alphaOrderMap[words[j][index]]
+		}
+		return len(words[i]) < len(words[j])
+	})
 }
