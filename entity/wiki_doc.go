005c210bfc47d2117c4f9de9a751d5b92b86b330
dinghenc <308024784@qq.com>
2022-05-23 23:44:19 +0800

    feat: wiki 词条结构

--- entity/wiki_doc.go
+++ entity/wiki_doc.go
@@ -2,4 +2,14 @@ package entity
 
 // WikiDocument wiki文档结构
 type WikiDocument struct {
+	Entries    []WikiEntry `json:"entries"`
+	ModifyTime int64       `json:"modify_time"`
+	CreateTime int64       `json:"create_time"`
+}
+
+// WikiEntry wiki词条
+type WikiEntry struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Text  string `json:"text"`
 }
